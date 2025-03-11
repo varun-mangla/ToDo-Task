737d9bcc02afd23452f288628bc6f7a623d51bcc
Varun Mangla <iamvarunmangla@gmail.com>
2025-03-11 23:53:36 +0530

    updated JS

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-alert("test")
+const todoForm=document.querySelector('form')
+const todoInput= document.getElementById('todo-input')
+const todoListUL= document.getElementById('todo-list')
+
+let allTodos=[];
+
+todoForm.addEventListener('submit',function(e){
+    alert('test')
+})
